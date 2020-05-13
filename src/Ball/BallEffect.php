1e50b344a2736094f2992c0bcac8523cbfe43c84
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-05-12 21:33:09 -0700

    Update BallEffect.php

--- src/Ball/BallEffect.php
+++ src/Ball/BallEffect.php
@@ -17,61 +17,54 @@ use pocketmine\entity\Entity;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\entity\EntityDamageEvent;
 
-class BallEffect extends PluginBase implements Listener{
+class BallEffect extends PluginBase implements Listener
+{
 
     public function onEnable()
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-}
-
+    }
  
     public function ItemHeld(PlayerItemHeldEvent $event)
-{
+    {
         $player = $event->getPlayer();
         $item = $event->getItem();
         if($item->getId() == Item::RABBIT_HIDE){
-
             $eff = new EffectInstance(Effect::getEffect(Effect::HASTE), 100 * 99999, 3, true);
             $player->addEffect($eff);
+        } else if ($player->hasEffect(Effect::LEVITATION) and (!$item->getId() == Item::RABBIT_HIDE)){
 
-   } else if ($player->hasEffect(Effect::LEVITATION) and (!$item->getId() == Item::RABBIT_HIDE)){
+            $player->removeEffect(25);
 
-    $player->removeEffect(25);
-   
-  } else {
-        $player->removeEffect(25);
+        } else {
+            $player->removeEffect(25);
 
- } 
-   }
+        }
+    }
 
-public function onDamage(EntityDamageEvent $event){
+    public function onDamage(EntityDamageEvent $event)
+    {
 
-              $player = $event->getEntity();
-              
-                       if($player instanceof Player){
- if($event->getEntity()->getInventory()->getItemInHand()->getId() === 415){
-if ($event->getCause() === EntityDamageEvent::CAUSE_FALL){
+        $player = $event->getEntity();
 
-        $event->setCancelled();
- 
-} else {
-} 
-} else {
-} 
-} else {
-} 
-} 
+        if($player instanceof Player){
+            if($event->getEntity()->getInventory()->getItemInHand()->getId() === 415){
+                if ($event->getCause() === EntityDamageEvent::CAUSE_FALL){
+                    $event->setCancelled();
+                }
+            }
+        }
+    }
 
 
-
-public function Leave(PlayerQuitEvent $event){
-          $player = $event->getPlayer();
+    public function Leave(PlayerQuitEvent $event)
+    {
+        $player = $event->getPlayer();
         $item = $player->getInventory()->getItemInHand();
         if($item->getId() == Item::RABBIT_HIDE){
-                $player->removeEffect(25);
- }
+            $player->removeEffect(25);
+        }
+    }
+
+
 }
-
-
-} 
